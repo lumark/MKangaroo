4586874368bfbe8d75a3a54a6bc0c65fe5fe1686
luma <lu.ma@colorado.edu>
2014-10-28 17:52:46 -0600

    add function to generate simple mesh (only faces and vertex) from grid sdf

--- kangaroo/GridSDF/MarchingCubesGrid.h
+++ kangaroo/GridSDF/MarchingCubesGrid.h
@@ -32,7 +32,8 @@ inline aiMesh* MeshFromLists(
     const std::vector<aiVector3D>&                    norms,
     const std::vector<aiFace>&                        faces,
     const std::vector<aiColor4D>&                     colors
-    ) {
+    )
+{
   aiMesh* mesh = new aiMesh();
   mesh->mPrimitiveTypes = aiPrimitiveType_TRIANGLE;
 
@@ -133,7 +134,7 @@ void vMarchCubeGrid(
     std::vector<aiVector3D>&                            norms,
     std::vector<aiFace>&                                faces,
     std::vector<aiColor4D>&                             colors,
-    float fTargetValue                                  = 0.0f
+    float                                               fTargetValue = 0.0f
     )
 {
   const float3 p = vol.VoxelPositionInUnits(x,y,z);
@@ -144,14 +145,14 @@ void vMarchCubeGrid(
   for(int iVertex = 0; iVertex < 8; iVertex++)
   {
     if(vol.CheckIfVoxelExist(
-         int(x+a2fVertexOffset[iVertex][0]),
-         int(y+a2fVertexOffset[iVertex][1]),
-         int(z+a2fVertexOffset[iVertex][2])) == true)
+         static_cast<int>(x+a2fVertexOffset[iVertex][0]),
+         static_cast<int>(y+a2fVertexOffset[iVertex][1]),
+         static_cast<int>(z+a2fVertexOffset[iVertex][2])) == true)
     {
       afCubeValue[iVertex] =
-          vol(int(x+a2fVertexOffset[iVertex][0]),
-          int(y+a2fVertexOffset[iVertex][1]),
-          int(z+a2fVertexOffset[iVertex][2]));
+          vol(static_cast<int>(x+a2fVertexOffset[iVertex][0]),
+          static_cast<int>(y+a2fVertexOffset[iVertex][1]),
+          static_cast<int>(z+a2fVertexOffset[iVertex][2]));
     }
     else
     {
@@ -245,6 +246,123 @@ void vMarchCubeGrid(
     faces.push_back(face);
   }
 }
+
+
+// =============================================================================
+//vMarchCube performs the Marching Cubes algorithm on a single cube
+// USER SHOULD MAKE SURE VOXEL EXIST!
+KANGAROO_EXPORT
+template<typename T, typename TColor>
+void vMarchCubeGridSimp(
+    BoundedVolumeGrid<T,roo::TargetHost, Manage>&       vol,
+    BoundedVolumeGrid<TColor,roo::TargetHost, Manage>&  volColor,
+    int x, int y, int z,
+    std::vector<aiVector3D>&                            verts,
+    std::vector<aiFace>&                                faces,
+    float fTargetValue                                  = 0.0f
+    )
+{
+  const float3 p = vol.VoxelPositionInUnits(x,y,z);
+  const float3 fScale = vol.VoxelSizeUnits();
+
+  //Make a local copy of the values at the cube's corners
+  float afCubeValue[8];
+  for(int iVertex = 0; iVertex < 8; iVertex++)
+  {
+    if(vol.CheckIfVoxelExist(
+         static_cast<int>(x+a2fVertexOffset[iVertex][0]),
+         static_cast<int>(y+a2fVertexOffset[iVertex][1]),
+         static_cast<int>(z+a2fVertexOffset[iVertex][2])) == true)
+    {
+      afCubeValue[iVertex] =
+          vol(static_cast<int>(x+a2fVertexOffset[iVertex][0]),
+          static_cast<int>(y+a2fVertexOffset[iVertex][1]),
+          static_cast<int>(z+a2fVertexOffset[iVertex][2]));
+    }
+    else
+    {
+      return;
+    }
+
+    if(!std::isfinite(afCubeValue[iVertex])) return;
+  }
+
+  //Find which vertices are inside of the surface and which are outside
+  int iFlagIndex = 0;
+  for(int iVertexTest = 0; iVertexTest < 8; iVertexTest++)
+  {
+    if(afCubeValue[iVertexTest] <= fTargetValue)
+      iFlagIndex |= 1<<iVertexTest;
+  }
+
+  //Find which edges are intersected by the surface
+  int iEdgeFlags = aiCubeEdgeFlags[iFlagIndex];
+
+  // If the cube is entirely inside or outside of the surface,
+  // then there will be no intersections
+  if(iEdgeFlags == 0) {
+    return;
+  }
+
+  //Find the point of intersection of the surface with each edge
+  //Then find the normal to the surface at those points
+  float3 asEdgeVertex[12];
+  float3 asEdgeNorm[12];
+
+  for(int iEdge = 0; iEdge < 12; iEdge++)
+  {
+    //if there is an intersection on this edge
+    if(iEdgeFlags & (1<<iEdge))
+    {
+      float fOffset = fGetOffset(afCubeValue[ a2iEdgeConnection[iEdge][0] ],
+          afCubeValue[ a2iEdgeConnection[iEdge][1] ], fTargetValue);
+
+      asEdgeVertex[iEdge] = make_float3(
+            p.x + (a2fVertexOffset[ a2iEdgeConnection[iEdge][0] ][0]  +
+          fOffset * a2fEdgeDirection[iEdge][0]) * fScale.x,
+          p.y + (a2fVertexOffset[ a2iEdgeConnection[iEdge][0] ][1]  +
+          fOffset * a2fEdgeDirection[iEdge][1]) * fScale.y,
+          p.z + (a2fVertexOffset[ a2iEdgeConnection[iEdge][0] ][2]  +
+          fOffset * a2fEdgeDirection[iEdge][2]) * fScale.z );
+
+      const float3 deriv = vol.GetUnitsBackwardDiffDxDyDz( asEdgeVertex[iEdge] );
+      asEdgeNorm[iEdge] = deriv / length(deriv);
+
+      if( !std::isfinite(asEdgeNorm[iEdge].x) ||
+          !std::isfinite(asEdgeNorm[iEdge].y) ||
+          !std::isfinite(asEdgeNorm[iEdge].z) )
+      {
+        asEdgeNorm[iEdge] = make_float3(0,0,0);
+      }
+    }
+  }
+
+
+  // Draw the triangles that were found.  There can be up to five per cube
+  for(int iTriangle = 0; iTriangle < 5; iTriangle++)
+  {
+    if(a2iTriangleConnectionTable[iFlagIndex][3*iTriangle] < 0)
+      break;
+
+    aiFace face;
+    face.mNumIndices = 3;
+    face.mIndices = new unsigned int[face.mNumIndices];
+
+    for(int iCorner = 0; iCorner < 3; iCorner++)
+    {
+      int iVertex = a2iTriangleConnectionTable[iFlagIndex][3*iTriangle+iCorner];
+
+      face.mIndices[iCorner] = verts.size();
+      verts.push_back(aiVector3D(asEdgeVertex[iVertex].x,
+                                 asEdgeVertex[iVertex].y,
+                                 asEdgeVertex[iVertex].z) );
+    }
+
+    faces.push_back(face);
+  }
+}
+
+
 
 
 //////////////////////////////////////////
@@ -383,17 +501,31 @@ void GenMeshSingleGrid(
                                  j*vol.m_nVolumeGridRes + y,
                                  k*vol.m_nVolumeGridRes + z) == true)
         {
-          // get voxel index for each grid.
           roo::vMarchCubeGrid(vol, volColor,
                               i*vol.m_nVolumeGridRes + x,
                               j*vol.m_nVolumeGridRes + y,
                               k*vol.m_nVolumeGridRes + z,
                               verts, norms, faces, colors);
+
+          //          roo::vMarchCubeGridSimp(vol, volColor,
+          //                                  i*vol.m_nVolumeGridRes + x,
+          //                                  j*vol.m_nVolumeGridRes + y,
+          //                                  k*vol.m_nVolumeGridRes + z,
+          //                                  verts, faces);
         }
       }
     }
   }
 }
+
+class MarchingCUBERst
+{
+public:
+  std::vector<aiVector3D>   verts;
+  std::vector<aiVector3D>   norms;
+  std::vector<aiFace>       faces;
+  std::vector<aiColor4D>    colors;
+};
 
 
 // =============================================================================
@@ -407,10 +539,7 @@ void SaveMeshGrid(
 {
   double dTime = _Tic();
 
-  std::vector<aiVector3D>   verts;
-  std::vector<aiVector3D>   norms;
-  std::vector<aiFace>       faces;
-  std::vector<aiColor4D>    colors;
+  MarchingCUBERst ObjMesh;
 
   // scan each grid..
   int nNumSkip =0;
@@ -424,7 +553,7 @@ void SaveMeshGrid(
       {
         if(vol.CheckIfBasicSDFActive(vol.GetIndex(i,j,k)) == true)
         {
-          GenMeshSingleGrid(vol,volColor,i,j,k,verts, norms, faces, colors);
+          GenMeshSingleGrid(vol,volColor,i,j,k,ObjMesh.verts, ObjMesh.norms, ObjMesh.faces, ObjMesh.colors);
           nNumSave++;
         }
         else
@@ -434,11 +563,10 @@ void SaveMeshGrid(
       }
     }
   }
-
-  aiMesh* mesh = MeshFromLists(verts,norms,faces,colors);
-
   printf("Finish march cube grid sdf. Save %d grids. Skip %d grids. Use time %f; \n",
          nNumSave, nNumSkip, _Toc(dTime));
+
+  aiMesh* mesh = MeshFromLists(ObjMesh.verts,ObjMesh.norms,ObjMesh.faces,ObjMesh.colors);
 
   SaveMeshGridToFile(filename, mesh, "obj");
 }
