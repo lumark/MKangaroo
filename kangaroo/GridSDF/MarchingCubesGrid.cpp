b3a269cae015e2c26506634c0f996472db3fc10f
luma <lu.ma@colorado.edu>
2014-11-03 17:13:15 -0700

    clean MarchingCubesGrid.h/cpp

--- kangaroo/GridSDF/MarchingCubesGrid.cpp
+++ kangaroo/GridSDF/MarchingCubesGrid.cpp
@@ -19,61 +19,12 @@
 #include "stdio.h"
 #include "math.h"
 
-
 #include <kangaroo/Sdf.h>
 #include <kangaroo/GridSDF/SdfSmart.h>
 #include <kangaroo/GridSDF/MarchingCubesGrid.h>
 
 namespace roo
 {
-
-
-
-//template<typename T, typename TColor>
-//void SaveMeshGrid(std::string filename, BoundedVolumeGrid<T, TargetHost, Manage> vol,
-//                  BoundedVolumeGrid<TColor, TargetHost, Manage> volColor )
-//{
-//  printf("in SaveMeshGrid/cpp..\n");
-
-//  std::vector<aiVector3D> verts;
-//  std::vector<aiVector3D> norms;
-//  std::vector<aiFace> faces;
-//  std::vector<aiColor4D> colors;
-
-//  for(GLint iX = 0; iX < vol.Voxels().x-1; iX++) {
-//    for(GLint iY = 0; iY < vol.Voxels().y-1; iY++) {
-//      for(GLint iZ = 0; iZ < vol.Voxels().z-1; iZ++) {
-
-//        const unsigned int nIndex =
-//            int(floorf(iX/vol.m_nVolumeGridRes)) +
-//            vol.m_nWholeGridRes* (int(floorf(iY/vol.m_nVolumeGridRes))+
-//                                  vol.m_nWholeGridRes* int(floorf(iZ/vol.m_nVolumeGridRes)));
-
-//        if(vol.CheckIfBasicSDFActive(nIndex) == true)
-//        {
-//          float val = vol(int(iX),int(iY),int(iZ)).val;
-//          if(std::isfinite(val) && val !=0)
-//          {
-//            if(val > 0.0000001 || val > -0.0000001 )
-//            {
-//              roo::vMarchCubeGrid(vol, volColor, iX,iY,iZ, verts, norms, faces, colors);
-//              printf("Finish march cube grid for (%d). val %f\n",int(iX), val);
-//            }
-//          }
-//        }
-//      }
-//    }
-//  }
-
-//  printf("finish march cube grid..\n");
-
-//  aiMesh* mesh = MeshFromLists(verts,norms,faces,colors);
-//  SaveMeshGrid(filename, mesh);
-//}
-
-
-
-
 // Instantiate templates
 //template void SaveMeshGrid<roo::SDF_t,float>(std::string, BoundedVolumeGrid<SDF_t,TargetHost, Manage> vol,
 //BoundedVolumeGrid<float,TargetHost, Manage> volColor);
@@ -81,4 +32,5 @@ namespace roo
 template void SaveMeshGrid<roo::SDF_t_Smart,float, Manage>(std::string,
 BoundedVolumeGrid<SDF_t_Smart,TargetHost, Manage> vol,
 BoundedVolumeGrid<float,TargetHost, Manage> volColor);
+
 }
--- kangaroo/GridSDF/MarchingCubesGrid.h
+++ kangaroo/GridSDF/MarchingCubesGrid.h
@@ -69,6 +69,7 @@ inline aiMesh* MeshFromLists(
 }
 
 // =============================================================================
+// here also support .obj file.
 KANGAROO_EXPORT
 inline void SaveMeshGridToFile(
     std::string         sFilename,
@@ -248,125 +249,9 @@ void vMarchCubeGrid(
 }
 
 
-// =============================================================================
-//vMarchCube performs the Marching Cubes algorithm on a single cube
-// USER SHOULD MAKE SURE VOXEL EXIST!
-KANGAROO_EXPORT
-template<typename T, typename TColor>
-void vMarchCubeGridSimp(
-    BoundedVolumeGrid<T,roo::TargetHost, Manage>&       vol,
-    BoundedVolumeGrid<TColor,roo::TargetHost, Manage>&  volColor,
-    int x, int y, int z,
-    std::vector<aiVector3D>&                            verts,
-    std::vector<aiFace>&                                faces,
-    float fTargetValue                                  = 0.0f
-    )
-{
-  const float3 p = vol.VoxelPositionInUnits(x,y,z);
-  const float3 fScale = vol.VoxelSizeUnits();
-
-  //Make a local copy of the values at the cube's corners
-  float afCubeValue[8];
-  for(int iVertex = 0; iVertex < 8; iVertex++)
-  {
-    if(vol.CheckIfVoxelExist(
-         static_cast<int>(x+a2fVertexOffset[iVertex][0]),
-         static_cast<int>(y+a2fVertexOffset[iVertex][1]),
-         static_cast<int>(z+a2fVertexOffset[iVertex][2])) == true)
-    {
-      afCubeValue[iVertex] =
-          vol(static_cast<int>(x+a2fVertexOffset[iVertex][0]),
-          static_cast<int>(y+a2fVertexOffset[iVertex][1]),
-          static_cast<int>(z+a2fVertexOffset[iVertex][2]));
-    }
-    else
-    {
-      return;
-    }
-
-    if(!std::isfinite(afCubeValue[iVertex])) return;
-  }
-
-  //Find which vertices are inside of the surface and which are outside
-  int iFlagIndex = 0;
-  for(int iVertexTest = 0; iVertexTest < 8; iVertexTest++)
-  {
-    if(afCubeValue[iVertexTest] <= fTargetValue)
-      iFlagIndex |= 1<<iVertexTest;
-  }
-
-  //Find which edges are intersected by the surface
-  int iEdgeFlags = aiCubeEdgeFlags[iFlagIndex];
-
-  // If the cube is entirely inside or outside of the surface,
-  // then there will be no intersections
-  if(iEdgeFlags == 0) {
-    return;
-  }
-
-  //Find the point of intersection of the surface with each edge
-  //Then find the normal to the surface at those points
-  float3 asEdgeVertex[12];
-  float3 asEdgeNorm[12];
-
-  for(int iEdge = 0; iEdge < 12; iEdge++)
-  {
-    //if there is an intersection on this edge
-    if(iEdgeFlags & (1<<iEdge))
-    {
-      float fOffset = fGetOffset(afCubeValue[ a2iEdgeConnection[iEdge][0] ],
-          afCubeValue[ a2iEdgeConnection[iEdge][1] ], fTargetValue);
-
-      asEdgeVertex[iEdge] = make_float3(
-            p.x + (a2fVertexOffset[ a2iEdgeConnection[iEdge][0] ][0]  +
-          fOffset * a2fEdgeDirection[iEdge][0]) * fScale.x,
-          p.y + (a2fVertexOffset[ a2iEdgeConnection[iEdge][0] ][1]  +
-          fOffset * a2fEdgeDirection[iEdge][1]) * fScale.y,
-          p.z + (a2fVertexOffset[ a2iEdgeConnection[iEdge][0] ][2]  +
-          fOffset * a2fEdgeDirection[iEdge][2]) * fScale.z );
-
-      const float3 deriv = vol.GetUnitsBackwardDiffDxDyDz( asEdgeVertex[iEdge] );
-      asEdgeNorm[iEdge] = deriv / length(deriv);
-
-      if( !std::isfinite(asEdgeNorm[iEdge].x) ||
-          !std::isfinite(asEdgeNorm[iEdge].y) ||
-          !std::isfinite(asEdgeNorm[iEdge].z) )
-      {
-        asEdgeNorm[iEdge] = make_float3(0,0,0);
-      }
-    }
-  }
-
-
-  // Draw the triangles that were found.  There can be up to five per cube
-  for(int iTriangle = 0; iTriangle < 5; iTriangle++)
-  {
-    if(a2iTriangleConnectionTable[iFlagIndex][3*iTriangle] < 0)
-      break;
-
-    aiFace face;
-    face.mNumIndices = 3;
-    face.mIndices = new unsigned int[face.mNumIndices];
-
-    for(int iCorner = 0; iCorner < 3; iCorner++)
-    {
-      int iVertex = a2iTriangleConnectionTable[iFlagIndex][3*iTriangle+iCorner];
-
-      face.mIndices[iCorner] = verts.size();
-      verts.push_back(aiVector3D(asEdgeVertex[iVertex].x,
-                                 asEdgeVertex[iVertex].y,
-                                 asEdgeVertex[iVertex].z) );
-    }
-
-    faces.push_back(face);
-  }
-}
-
-
-
 
 //////////////////////////////////////////
-// Save SDF
+/// Save SDF
 //////////////////////////////////////////
 // =============================================================================
 KANGAROO_EXPORT
@@ -506,12 +391,6 @@ void GenMeshSingleGrid(
                               j*vol.m_nVolumeGridRes + y,
                               k*vol.m_nVolumeGridRes + z,
                               verts, norms, faces, colors);
-
-          //          roo::vMarchCubeGridSimp(vol, volColor,
-          //                                  i*vol.m_nVolumeGridRes + x,
-          //                                  j*vol.m_nVolumeGridRes + y,
-          //                                  k*vol.m_nVolumeGridRes + z,
-          //                                  verts, faces);
         }
       }
     }
@@ -553,7 +432,8 @@ void SaveMeshGrid(
       {
         if(vol.CheckIfBasicSDFActive(vol.GetIndex(i,j,k)) == true)
         {
-          GenMeshSingleGrid(vol,volColor,i,j,k,ObjMesh.verts, ObjMesh.norms, ObjMesh.faces, ObjMesh.colors);
+          GenMeshSingleGrid(vol,volColor,i,j,k,ObjMesh.verts,
+                            ObjMesh.norms, ObjMesh.faces, ObjMesh.colors);
           nNumSave++;
         }
         else
@@ -566,12 +446,11 @@ void SaveMeshGrid(
   printf("Finish march cube grid sdf. Save %d grids. Skip %d grids. Use time %f; \n",
          nNumSave, nNumSkip, _Toc(dTime));
 
-  aiMesh* mesh = MeshFromLists(ObjMesh.verts,ObjMesh.norms,ObjMesh.faces,ObjMesh.colors);
+  aiMesh* mesh = MeshFromLists(ObjMesh.verts,ObjMesh.norms,
+                               ObjMesh.faces,ObjMesh.colors);
 
   SaveMeshGridToFile(filename, mesh, "obj");
 }
-
-
 
 
 // =============================================================================
@@ -581,6 +460,7 @@ void SaveMeshGridSepreate(
     std::string                                       filename,
     const BoundedVolumeGrid<T,TargetHost,Manage>      vol,
     const BoundedVolumeGrid<TColor,TargetHost,Manage> volColor );
+
 
 // =============================================================================
 KANGAROO_EXPORT
