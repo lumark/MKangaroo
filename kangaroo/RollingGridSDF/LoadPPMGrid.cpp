530423ab5bac86364d2da306762a9084c436be30
luma <lu.ma@colorado.edu>
2015-01-20 22:56:31 -0700

    add loadPPMGrid.cpp

---
+++ kangaroo/RollingGridSDF/LoadPPMGrid.cpp
@@ -0,0 +1,31 @@
+#include "LoadPPMGrid.h"
+
+KANGAROO_EXPORT
+roo::BoundingBox LoadPXMBoundingBox(std::string filename)
+{
+  std::ifstream bFile( filename.c_str(), std::ios::in | std::ios::binary );
+  if(bFile.fail()==true)
+  {
+    std::cerr<<"Fatal error! file "<<filename<<" does not exist."<<std::endl;
+    exit(-1);
+  }
+
+  //read in the bounding volume bounds
+  roo::BoundingBox BBox;
+
+  bFile >> BBox.boxmin.x;
+  bFile >> BBox.boxmin.y;
+  bFile >> BBox.boxmin.z;
+  bFile >> BBox.boxmax.x;
+  bFile >> BBox.boxmax.y;
+  bFile >> BBox.boxmax.z;
+  bFile.ignore(1,'\n');
+
+  //  printf("Load BB success. BBMin:(%f,%f,%f), BBMax:(%f,%f,%f) \n",
+  //         BBox.boxmin.x, BBox.boxmin.y,BBox.boxmin.z,
+  //         BBox.boxmax.x, BBox.boxmax.y, BBox.boxmax.z);
+
+  bFile.close();
+  return BBox;
+}
+
--- kangaroo/RollingGridSDF/LoadPPMGrid.h
+++ kangaroo/RollingGridSDF/LoadPPMGrid.h
@@ -15,9 +15,9 @@
 // P5	Portable graymap	Binary
 // P6	Portable pixmap	Binary
 
-/////////////////////////////////////////////////////////////////////////////
-// Load Volume types
-/////////////////////////////////////////////////////////////////////////////
+KANGAROO_EXPORT
+roo::BoundingBox LoadPXMBoundingBox(std::string filename);
+
 KANGAROO_EXPORT
 template<typename T>
 bool LoadPXMSingleGrid(
@@ -68,35 +68,6 @@ bool LoadPXMSingleGrid(
 
   bFile.close();
   return success;
-}
-
-KANGAROO_EXPORT
-inline roo::BoundingBox LoadPXMBoundingBox(std::string filename)
-{
-  std::ifstream bFile( filename.c_str(), std::ios::in | std::ios::binary );
-  if(bFile.fail()==true)
-  {
-    std::cerr<<"Fatal error! file "<<filename<<" does not exist."<<std::endl;
-    exit(-1);
-  }
-
-  //read in the bounding volume bounds
-  roo::BoundingBox BBox;
-
-  bFile >> BBox.boxmin.x;
-  bFile >> BBox.boxmin.y;
-  bFile >> BBox.boxmin.z;
-  bFile >> BBox.boxmax.x;
-  bFile >> BBox.boxmax.y;
-  bFile >> BBox.boxmax.z;
-  bFile.ignore(1,'\n');
-
-  //  printf("Load BB success. BBMin:(%f,%f,%f), BBMax:(%f,%f,%f) \n",
-  //         BBox.boxmin.x, BBox.boxmin.y,BBox.boxmin.z,
-  //         BBox.boxmax.x, BBox.boxmax.y, BBox.boxmax.z);
-
-  bFile.close();
-  return BBox;
 }
 
 KANGAROO_EXPORT
