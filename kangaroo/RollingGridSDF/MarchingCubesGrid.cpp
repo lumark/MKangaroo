7b1867e45682d8b42f926b34ca5f77313d371bea
luma <lu.ma@colorado.edu>
2015-01-21 09:02:19 -0700

    add MarchingCubesGrid.cpp

--- kangaroo/RollingGridSDF/MarchingCubesGrid.cpp
+++ kangaroo/RollingGridSDF/MarchingCubesGrid.cpp
@@ -1,1 +1,17 @@
+#include "MarchingCubesGrid.h"
 
+
+// fGetOffset finds the approximate point of intersection of the surface
+// between two points with the values fValue1 and fValue2
+KANGAROO_EXPORT
+float fGetOffset(
+    float                                             fValue1,
+    float                                             fValue2,
+    float                                             fValueDesired)
+{
+  const double fDelta = fValue2 - fValue1;
+  if(fDelta == 0.0) {
+    return 0.5;
+  }
+  return (fValueDesired - fValue1)/fDelta;
+}
--- kangaroo/RollingGridSDF/MarchingCubesGrid.h
+++ kangaroo/RollingGridSDF/MarchingCubesGrid.h
@@ -21,17 +21,10 @@ public:
 // fGetOffset finds the approximate point of intersection of the surface
 // between two points with the values fValue1 and fValue2
 KANGAROO_EXPORT
-inline float fGetOffset(
+float fGetOffset(
     float                                             fValue1,
     float                                             fValue2,
-    float                                             fValueDesired)
-{
-  const double fDelta = fValue2 - fValue1;
-  if(fDelta == 0.0) {
-    return 0.5;
-  }
-  return (fValueDesired - fValue1)/fDelta;
-}
+    float                                             fValueDesired);
 
 // vMarchCube performs the Marching Cubes algorithm on a single cube
 // USER SHOULD MAKE SURE VOXEL EXIST!
